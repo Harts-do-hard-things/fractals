f3055e85f5ac57fdc5d2d9023dca671b2c57ba06
cub525 <49500036+cub525@users.noreply.github.com>
2020-11-25 17:06:36 -0700

    Update fractal.py

--- fractal.py
+++ fractal.py
@@ -19,7 +19,7 @@ class Fractal:
         self.plot_list = []
         self.angle = [0]
         self.i = 0
-    
+
     def iterate(self, i):
         for j in range(i):
             S = []
@@ -27,7 +27,15 @@ class Fractal:
                 S.extend(list(map(func,self.S)))
                 S.append(float('nan'))
             self.S = S
-            
+
+    def iterate_return_points(self, i):
+        for j in range(i):
+            S = []
+            for func in self.func_list:
+                S.extend(list(map(func,self.S)))
+            self.S = S
+        self. S = [item for sublist in [(S[i],S[i+1],math.nan) for i in range(len(S)) if i%2 == 0] for item in sublist]
+
     def rotate(self,angle):
         for k in self.angle:
             S = [i*cmath.exp(k*1j) for i in self.S]
@@ -38,16 +46,16 @@ class Fractal:
         fig = plt.figure()
         ax = fig.add_subplot(111)
         # Previously was naming this variable self.plot which breaks self.plot()
-        for S in self.plot_list:
-            self.plot_handle = ax.plot(np.real(S),np.imag(S))
+        # for S in self.plot_list:
+        self.plot_handle = ax.plot(np.real(self.S),np.imag(self.S))
         plt.axis('equal')
-        
-    @gif.frame    
+
+    @gif.frame
     def gif_plot(self):
         plt.plot(np.real(self.S),np.imag(self.S))
         plt.ylim((-1,1))
         plt.axis('equal')
-        
+
     def save_gif(self,path,iterations,duration = 1000):
         frames = []
         for i in range(iterations):
@@ -55,16 +63,17 @@ class Fractal:
             frame = self.gif_plot()
             frames.append(frame)
         gif.save(frames, '{0}_{1}.gif'.format(path,iterations), duration)
-        
 
-# Another option for making the code cleaner... 
+
+# Another option for making the code cleaner...
 class HeighwayDragon(Fractal):
     def __init__(self):
         Fractal.__init__(self,
                          S0 = [0,1],
-                         func_list = 
-                         [lambda z :  0.5*(1+1j)*z, lambda z : 1-0.5*(1-1j)*z])
-        
+                         func_list =
+                         [lambda z :  0.5*(1+1j)*z,
+                          lambda z : 1-0.5*(1-1j)*z])
+
 lamda = .5 - 1j/2/math.sqrt(3)
 lamdaconj = .5 + 1j/2/math.sqrt(3)
 
@@ -72,15 +81,15 @@ class fudgeflake(Fractal):
     def __init__(self):
         Fractal.__init__(self,
                          S0 = [0,1],
-                         func_list = 
-                         [lambda z :  lamda*z, 
+                         func_list =
+                         [lambda z :  lamda*z,
                           lambda z : 1j/math.sqrt(3)*z+lamda,
-                          lambda z : lamda*z + lamdaconj])       
+                          lambda z : lamda*z + lamdaconj])
 
 SF =[]
-S0 = [0,1]
+S0i = [0,1]
 
-#Vars Used in terdragon    
+#Vars Used in terdragon
 
 
 #Vars used in twindragon
@@ -118,11 +127,13 @@ SK = [-1,0]
 
 
 
-# This seems like a cleaner way to make the IFS functions... Probably as 
+# This seems like a cleaner way to make the IFS functions... Probably as
 # members of the class
 IFS_function = dict()
 # Plain Ole' Dragon Curve
-IFS_function['dragon'] = [lambda z :  0.5*(1+1j)*z, lambda z : 1-0.5*(1-1j)*z]
+IFS_function['dragon'] = [
+    lambda z :  0.5*(1+1j)*z,
+    lambda z : 1-0.5*(1-1j)*z]
 #z2 dragon curve
 IFS_function['z2_dragon'] = [
         lambda z :  0.5*(1+1j)*z,
@@ -133,14 +144,14 @@ IFS_function['z2_dragon'] = [
 # Levy C
 def func1(z):
     return 0.5*(1-1j)*z
-    
+
 def func2(z):
     return 1+0.5*(1+1j)*(z-1)
 
 # Twin Dragon
 def twin1(z):
     return 0.5*(1+1j)*z
-    
+
 def twin2(z):
     return 1-0.5*(1+1j)*z
 
@@ -155,9 +166,9 @@ def ter3(z):
 #golden dragon
 
 def gd1(z):
-    
+
     return r*z*cmath.exp(A*1j)
-    
+
 def gd2(z):
     return r**2*z*cmath.exp((math.pi-B)*1j)+1
 
@@ -168,12 +179,12 @@ def z2gd1(z):
 
 def z2gd2(z):
     return r*z*cmath.exp((A-math.pi)*1j)
-    
+
 def z2gd3(z):
-    return gd2(z)  
-    
+    return gd2(z)
+
 def z2gd4(z):
-    return r**2*z*cmath.exp(-B*1j)-1    
+    return r**2*z*cmath.exp(-B*1j)-1
 
 #z2 levy curve
 
@@ -182,10 +193,10 @@ def z2levy1(z):
 
 def z2levy2(z):
     return -(1+0.5*(1+1j)*(z-1))
-    
+
 def z2levy3(z):
-    return func2(z)  
-    
+    return func2(z)
+
 def z2levy4(z):
     return -(0.5*(1-1j)*z)
 
@@ -253,19 +264,19 @@ pentagon = np.cumsum(star)
 
 
 if __name__ == "__main__":
-    # dragon = Fractal(S0,IFS_function['dragon'])
+    # dragon = Fractal(S0i,IFS_function['dragon'])
     # dragon.save_gif('Dragon',18)
     # dragon.iterate(5)
     # dragon.rotate(- math.pi*.5)
     # dragon.plot()
-    # heighway = HeighwayDragon()
-    # heighway.iterate(17)
-    # heighway.plot()
-    # levy = Fractal(S0,[func1,func2])
+    heighway = HeighwayDragon()
+    heighway.iterate(17)
+    heighway.plot()
+    # levy = Fractal(S0i,[func1,func2])
     # levy.save_gif('levy_C', 19)
     # levy.iterate(17)
     # levy.plot()
-    # z2_dragon = Fractal(S0,IFS_function['z2_dragon'])
+    # z2_dragon = Fractal(S0i,IFS_function['z2_dragon'])
     # z2_dragon.save_gif('z2_dragon',10)
     # z2_dragon.iterate(10)
     # z2_dragon.plot()
@@ -273,23 +284,23 @@ if __name__ == "__main__":
     # z2levy.save_gif('z2levy', 10)
     # z2levy.iterate(10)
     # z2levy.plot()
-    # twin_dragon = Fractal(S0_twin,[twin1,twin2])
+    # twin_dragon = Fractal(S0i_twin,[twin1,twin2])
     # twin_dragon.save_gif('Twin dragon', 17)
     # twin_dragon.iterate(17)
     # twin_dragon.plot()
-    # terdragon = Fractal(S0,[ter1,ter2,ter3])
+    # terdragon = Fractal(S0i,[ter1,ter2,ter3])
     # terdragon.save_gif('terdragon',12)
     # terdragon.rotate([math.pi/3,2*math.pi/3])
     # terdragon.plot()
-    # golden_dragon = Fractal(S0,[gd1,gd2])
+    # golden_dragon = Fractal(S0i,[gd1,gd2])
     # golden_dragon.save_gif('Golden_Dragon', 21)
     # golden_dragon.iterate(21)
     # golden_dragon.plot()
-    z2_golden_dragon = Fractal(S0,[z2gd1,z2gd2,z2gd3,z2gd4])
-    z2_golden_dragon.save_gif('z2_golden_dragon',10)
+    # z2_golden_dragon = Fractal(S0i,[z2gd1,z2gd2,z2gd3,z2gd4])
+    # z2_golden_dragon.save_gif('z2_golden_dragon',10)
     # z2_golden_dragon.iterate(10)
     # z2_golden_dragon.plot()
-    # pentigree = Fractal(S0,[pent1,pent2,pent3,pent4,pent5,pent6])
+    # pentigree = Fractal(S0i,[pent1,pent2,pent3,pent4,pent5,pent6])
     # pentigree.save_gif('Pentigree', 8)
     # pentigree.iterate(8)
     # pentigree.plot()
@@ -297,9 +308,8 @@ if __name__ == "__main__":
     # pentadentrite.save_gif('Pentadentrite', 8)
     # pentadentrite.iterate(8)
     # pentadentrite.plot()
-    Koch_fake = Fractal(S0,[koch1,koch2,koch3,koch4])
-    Koch_fake.save_gif('Koch_flake', 10)
+    # Koch_fake = Fractal(S0i,[koch1,koch2,koch3,koch4])
+    # Koch_fake.save_gif('Koch_flake', 10)
     # Koch_fake.iterate(3)
     # Koch_fake.plot()
     pass
-    
