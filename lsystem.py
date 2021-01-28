fdb4a054f5ac429e20b78e28c1cd0a830b625fa3
cub525 <49500036+cub525@users.noreply.github.com>
2021-01-28 08:01:26 -0700

    Add animations

--- lsystem.py
+++ lsystem.py
@@ -13,19 +13,18 @@ import turtle
 
 @dtcls.dataclass()
 class L_System:
-    """L-System constructor for Fractals"""
+    
     axiom: str
     angle: float
     rules: Dict[str, str]
-    
     def __post_init__(self):
         self.commands = {
         'F': lambda t:t.forward(20),
         'G': lambda t:t.forward(20),
         'Z': lambda _: None,
         '+': lambda t:t.right(self.angle),
-        '-': lambda t:t.left(self.angle),
-        '−': lambda t:t.left(self.angle)
+        '-': lambda t:t.left(self.angle)
+        # '−': lambda t:t.left(self.angle)
         }
         self.rules = str.maketrans(self.rules)
         self.state = self.axiom
@@ -36,23 +35,25 @@ class L_System:
     
     def plot(self):
         t = turtle.Turtle()
-        turtle.tracer(0, 0)
+        # turtle.tracer(0, 0)
         for c in self.state:
             if c in self.commands:
                 self.commands[c](t)
-        turtle.update()
+        # turtle.update()
         turtle.mainloop()
         turtle.bye()
 
     def __str__(self):
+        n1='\n'
         return f"""Angle {self.angle}
 Axiom {self.axiom}
-{''.join([f'{chr(key)} —> {self.rules[key]}' for key in self.rules])}"""
+{''.join([f'{chr(key)} --> {self.rules[key]}' + n1 for key in self.rules])}"""
 
     @classmethod
     def from_input(cls, string = None):
         if not string:
             string = input("Paste the fractal information:")
+        # string.translate(str.maketrans({'−':'-'}))
         info_list = string.splitlines()
         angle = int(info_list[0][5:])
         axiom = info_list[1][6:]
@@ -61,10 +62,10 @@ Axiom {self.axiom}
         return f
 
 
-
-flowsnake = L_System.from_input("""Angle 60
-Axiom F
-F—>F−G−−G+F++FF+G−
-G—>+F−GG−−G−F++F+G""")
-flowsnake.iterate(3)
-flowsnake.plot()
+if __name__ == '__main__':
+    flowsnake = L_System('F',70,{'F':"+F--F+"}) #L_System.from_input("""Angle 60
+    # Axiom F
+    # F->F-G--G+F++FF+G-
+    # G->+F-GG--G-F++F+G""")
+    flowsnake.iterate(7)
+    flowsnake.plot()
